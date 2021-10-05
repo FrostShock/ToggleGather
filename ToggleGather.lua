43d2d055e13671df58bc8960e42f64595a3c8bc7
FrostShock <46273569+FrostShock@users.noreply.github.com>
2021-10-06 01:03:56 +0200

    Update ToggleGather.lua

--- ToggleGather.lua
+++ ToggleGather.lua
@@ -1,22 +1,14 @@
 function ToggleGather()
-  seconds2 = GetTime();
-  seconds = seconds2 - seconds1;
-  seconds1 = seconds2;
-  if (seconds > 1) then
-    find = 1 - find;
-    if (find == 1) then
-      CastSpellByName("Find Minerals");
-    else
-      CastSpellByName("Find Herbs");
-    end;
-  end;
---  echo(seconds);
+  if GetTrackingTexture() == "Interface\\Icons\\Spell_Nature_Earthquake" then
+    CastSpellByName("Find Herbs");
+  else
+    CastSpellByName("Find Minerals");
+  end
+--   MyTime = GetTime();
+--   echo(MyTime);
 end
 
-find = 0;
-seconds1 = GetTime();
-
--------------- bindings
+-- Key Bindings
 BINDING_HEADER_TOGGLEGATHER = "Toggle Gather"
 
 BINDING_NAME_TOGGLEGATHER = "Toggle Finding Herbs / Minerals"
